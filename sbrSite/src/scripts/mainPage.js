23f08450e76e7c9a29eb4eb088cbc03897abe473
Vasiliy <SBRDigital@yandex.ru>
2024-11-13 16:09:35 +0300

    добавил сайтец

---
+++ sbrSite/src/scripts/mainPage.js
@@ -0,0 +1,28 @@
+const { createApp } = Vue;
+import { ScriptsUtils } from './utils.js'
+
+
+createApp({
+    data() {
+        const _description = 'Чат-боты для бизнеса'
+        const sbr = 'SBR DIGITAL'
+        const imgDir = './src/img/'
+        const tgImgSrc = imgDir + 'tg.png'
+        const tgPubUrl = 'https://t.me/+7yaJPC70S85lM2Qy'
+        const dataCollectorScriptUrl = 'https://sbrdigital.github.io/dcjs_cdn/dcCollectorBundle.js'
+
+        return {
+            title: sbr,
+            heading: sbr,
+            description: _description,
+            link: tgPubUrl,
+            imageSrc: tgImgSrc,
+            imageAlt: sbr + ' ' + _description,
+            dataCollectorScriptUrl: dataCollectorScriptUrl
+        };
+    },
+    async mounted() {
+        document.title = this.title;  // Заголовок сайта
+        ScriptsUtils.createScript('dcUrlScript', true, this.dataCollectorScriptUrl)
+    }
+}).mount('#app');
